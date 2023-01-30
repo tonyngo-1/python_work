ab7347863b54efb2d0c1237d71ead6c4fa0112db
Tony Ngo <tony@Tonys-MBP.lan1>
2023-01-30 17:32:20 -0500

    URL Scan boilerplate

--- api_work.py
+++ api_work.py
@@ -1,27 +1,44 @@
 import time
 import config
 import requests
-import pprint
 
-intezer_api = config.INTEZER_KEY
-virustotal_api = config.VIRUSTOTAL_KEY
+intezer_key = config.INTEZER_KEY
+virustotal_key = config.VIRUSTOTAL_KEY
 
+# base url setup
 base_url = "https://analyze.intezer.com/api/v2-0"
+
+# API URL setup with key using requests library
 response = requests.post(
     base_url + "/get-access-token",
-    json={"api_key": intezer_api},
+    json={"api_key": intezer_key},
 )
+
+# API authentication
 response.raise_for_status()
 session = requests.session()
 session.headers["Authorization"] = session.headers["Authorization"] = (
     "Bearer %s" % response.json()["result"]
 )
 
+# Malicious URL Scan
+malicious_url = input("Enter malicious URL: ")
+params = {"url": malicious_url}
+response = session.post(base_url + "/url", params=params)
+
+# Malicious URL Scan Result
+verdict = "/url/0833e33b-2dcd-4d48-a853-8b4822675911"
+
+
+print(response.json())
+
+# Malicious File upload
 # with open("malicious.sample", "rb") as file_to_upload:
 #     files = {"file": ("file_name", file_to_upload)}
 #     response = session.post(base_url + "/analyze", files=files)
 #     assert response.status_code == 201
 
+# I
 # while response.status_code != 200:
 #     time.sleep(1)
 #     result_url = response.json()["result_url"]
@@ -29,4 +46,5 @@ session.headers["Authorization"] = session.headers["Authorization"] = (
 #     response.raise_for_status()
 
 # report = response.json()
-# pprint.pprint(report)
+# print(report)
+# # pprint.pprint(report)
